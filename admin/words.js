9d1824c423b8c3005d6351476939f3b4acb710cd
Korbinian Zehentner <korbinian.zehentner@stud.th-rosenheim.de>
2022-04-15 19:04:13 +0200

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'fernseher adapter settings': {
+		en: 'Adapter settings for fernseher',
+		de: 'Adaptereinstellungen für fernseher',
+		ru: 'Настройки адаптера для fernseher',
+		pt: 'Configurações do adaptador para fernseher',
+		nl: 'Adapterinstellingen voor fernseher',
+		fr: "Paramètres d'adaptateur pour fernseher",
+		it: "Impostazioni dell'adattatore per fernseher",
+		es: 'Ajustes del adaptador para fernseher',
+		pl: 'Ustawienia adaptera dla fernseher',
+		'zh-cn': 'fernseher的适配器设置',
+	},
+	option1: {
+		en: 'option1',
+		de: 'Option 1',
+		ru: 'Опция 1',
+		pt: 'Opção 1',
+		nl: 'Optie 1',
+		fr: 'Option 1',
+		it: 'opzione 1',
+		es: 'Opción 1',
+		pl: 'opcja 1',
+		'zh-cn': '选项1',
+	},
+	option2: {
+		en: 'option2',
+		de: 'Option 2',
+		ru: 'вариант 2',
+		pt: 'opção 2',
+		nl: 'Optie 2',
+		fr: 'Option 2',
+		it: 'opzione 2',
+		es: 'opcion 2',
+		pl: 'Opcja 2',
+		'zh-cn': '选项2',
+	},
+};
